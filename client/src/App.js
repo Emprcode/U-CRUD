fec0505cbae7b25d4962cd1e977a7db70ba24d1d
Narayan Aryal <aryal.coding@gmail.com>
2023-01-19 12:18:19 +1100

    client folder recreated

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,23 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
-import { UserForm } from "./components/UserForm";
-import { UserTable } from "./components/UserTable";
-import { useEffect, useState } from "react";
-import { ToastContainer } from "react-toastify";
-import { fetchUser } from "./components/helper/axiosHelper";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
-  const [userList, setUserList] = useState([]);
-
-  const getUser = async () => {
-    const { result } = await fetchUser();
-    setUserList(result);
-  };
-
-  useEffect(() => {
-    getUser();
-  }, []);
   return (
     <div className="App">
-      <section>
-        {/* userForm  */}
-        <Container>
-          <UserForm getUser={getUser} />
-          <hr />
-        </Container>
-      </section>
-      <section>
-        <Container className="mt-5">
-          <div> {userList.length} User Found!</div>
-          {/* table */}
-          <UserTable userList={userList} getUser={getUser} />
-        </Container>
-      </section>
-      <ToastContainer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
